3984e1a1c143d221c9eb96559652f4989ce1872e
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2024-03-06 13:33:49 +0900

    Create Title_Per_PV.sql

---
+++ PV関連/Title_Per_PV.sql
@@ -0,0 +1,16 @@
+/*
+記事ごとのページビューを集計（人気順）
+*/
+
+select
+  (select value.string_value from unnest(event_params) where key = 'page_title') as page_title,
+  count(*) as pv_count
+from
+  `ga4-bigquery-pj-392206.analytics_342165491.events_*`
+where
+  _table_suffix between '20240201' and '20240229'
+  and event_name = 'page_view'
+group by
+  page_title
+order by
+  pv_count desc
