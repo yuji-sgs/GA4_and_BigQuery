4ab91337b6372748afbb277c108e7597199a2e5a
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2024-03-08 17:42:35 +0900

    Create Count_Monthly_PV.sql

---
+++ PV関連/Count_Monthly_PV.sql
@@ -0,0 +1,13 @@
+/*
+月次のPV数を集計
+*/
+
+select
+  date_trunc(parse_date("%Y%m%d", event_date), month) as month,
+  countif(event_name = 'page_view') as pv
+from
+  `ga4-bigquery-pj-392206.analytics_342165491.events_*`
+group by
+  month
+order by
+  month
