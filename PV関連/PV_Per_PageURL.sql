4e9a32911610460d9b34d000a29ca9e75af03af6
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2024-03-08 17:43:53 +0900

    Create PV_Per_PageURL.sql

---
+++ PV関連/PV_Per_PageURL.sql
@@ -0,0 +1,33 @@
+/*
+ページURLごとのPV数を集計
+*/
+
+-- UDF
+create temporary function 
+   get_value(params any type,name string ) as  
+   ( ( 
+       select 
+           coalesce( 
+               value.string_value 
+               , cast(value.int_value as string) 
+               , cast(value.float_value as string) 
+               , cast(value.double_value as string) 
+               ) 
+       from 
+           unnest(params) as x 
+       where 
+           x.key = name 
+   ) ); 
+
+select 
+   regexp_extract(lower(get_value(event_params, 'page_location')),r'^([^\?]+)') as page_location 
+   , count(1) as pv 
+from 
+   `ga4-bigquery-pj-392206.analytics_342165491.events_*`
+where 
+   _table_suffix between '20240201' and '20240229' 
+   and event_name = 'page_view' 
+group by 
+   page_location 
+order by
+   pv desc
